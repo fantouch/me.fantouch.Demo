7fa4b57d430ccb66eb51cae89bf9a572afc83bd4
fantouch <fantouchx@gmail.com>
2013-05-06 19:31:37 +0800

    me.fantouch.libs的Demo

---
+++ src/me/fantouch/demo/TestIndicativeRadioGpActivity.java
@@ -0,0 +1,31 @@
+
+package me.fantouch.demo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+import me.fantouch.libs.indicativeradio.IndicativeRadioGroup;
+
+public class TestIndicativeRadioGpActivity extends Activity {
+    private IndicativeRadioGroup mIndicativeRadioGroup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_indicative_radiogp);
+
+        mIndicativeRadioGroup = (IndicativeRadioGroup) findViewById(R.id.mIndicativeRadioGroup);
+    }
+
+    public void onClick(View v) {
+        int id = v.getId();
+
+        if (id == R.id.hide) {
+            mIndicativeRadioGroup.hide();
+        } else if (id == R.id.show) {
+            mIndicativeRadioGroup.show();
+        }
+    }
+
+}
