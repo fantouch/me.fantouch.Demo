6607e355efa61c474b32780e4cd80490bcaaa4f6
fantouch <fantouchx@gmail.com>
2013-05-20 15:04:23 +0800

    在测试用例里面才注册CrashHandler,使用法明了,按需使用.

--- src/me/fantouch/demo/TestCrashActivity.java
+++ src/me/fantouch/demo/TestCrashActivity.java
@@ -5,12 +5,17 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 
+import me.fantouch.libs.crash.CrashHandler;
+import me.fantouch.libs.log.Logg;
 
 
 public class TestCrashActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // 注册crashHandler,实际使用时建议在Application里面执行
+        CrashHandler.getInstance().init(getApplicationContext(), SendService.class);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_crash);
     }
